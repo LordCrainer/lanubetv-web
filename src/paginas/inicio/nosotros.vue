b5eaeee39d123f6d3ba88fba7d749aa443f53ab1
LordCrainer <camogan3000@hotmail.com>
2019-04-03 11:46:37 -0500

    Ajustando desde Samsung Pentium

--- src/paginas/inicio/nosotros.vue
+++ src/paginas/inicio/nosotros.vue
@@ -15,14 +15,14 @@
       </v-flex>
       <v-flex>
         <v-layout row wrap align-center justify-center>
-          <v-flex xs12  md10>
+          <v-flex xs12 md10>
             <v-card flat color="transparent">
               <v-flex xs12>
                 <v-container>
                   <v-layout align-center justify-center>
                     <v-card color="transparent" flat>
                       <v-card-text class="hidden-sm-and-down">
-                        <img contain width="100%"  :src="items.src1" alt>
+                        <img contain width="100%" :src="items.src1" alt>
                       </v-card-text>
                       <v-card-text class="hidden-md-and-up">
                         <img contain width="100%" style="max-height:600px" :src="items.src" alt>
@@ -33,7 +33,7 @@
               </v-flex>
             </v-card>
           </v-flex>
-          <v-flex xs12  md2>
+          <v-flex xs12 md2>
             <v-layout justify-center>
               <v-btn round dark class="mb-4 pa-4 " color="primary" to="/canalestv">CANALES TV</v-btn>
             </v-layout>
@@ -41,7 +41,7 @@
         </v-layout>
       </v-flex>
       <v-flex>
-        <v-layout row wrap justify-center >
+        <v-layout row wrap justify-center>
           <v-flex xs12 sm12 md10 class="pa-4">
             <v-layout row justify-center>
               <v-card-title class="headline white--text pa-0">
